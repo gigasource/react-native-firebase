89a09863b19195bfef996ae8c988c9c5fc22e8de
transse04731 <triracle@gmail.com>
2020-08-18 18:41:40 +0700

    set appGroup to global appGroup

--- packages/messaging/ios/RNFBMessaging/RNFBMessagingModule.m
+++ packages/messaging/ios/RNFBMessaging/RNFBMessagingModule.m
@@ -326,7 +326,7 @@ RCT_EXPORT_METHOD(unsubscribeFromTopic:
 }
 
 RCT_EXPORT_METHOD(getBackgroundMessage) {
-  NSUserDefaults *sharedDefault = [[NSUserDefaults alloc] initWithSuiteName:@"group.io.gigasource.notification"];
+  NSUserDefaults *sharedDefault = [[NSUserDefaults alloc] initWithSuiteName:appGroup];
   NSMutableArray *messArray = [NSMutableArray arrayWithArray:[sharedDefault objectForKey:@"notifications"]];
   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
     for (NSDictionary *message in messArray) {
